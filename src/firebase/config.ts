84be476b8b572ad1568c2641f52976e2eda8d256
Razab mondal <razabm28@gmail.com>
2026-02-05 15:30:20 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9271559579-27233",
+  "appId": "1:1059527376848:web:c51fa681168c84445843d5",
+  "apiKey": "AIzaSyC4hLMnHDGGYDRmh5LkvS-C6m9bkbewUVY",
+  "authDomain": "studio-9271559579-27233.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1059527376848"
+};
